70faa3113f65e6997501bf8f64c958a5e1898392
DennyAhn <aa5051140@gmail.com>
2025-10-19 22:57:30 +0900

    Fix: PostLikeId에 equals와 hashCode 메서드 추가
    - 복합 키의 동등성 비교를 위한 equals/hashCode 구현
    - 좋아요 토글 기능의 정상 작동을 위한 수정

--- src/main/java/com/fintech/server/community/entity/PostLikeId.java
+++ src/main/java/com/fintech/server/community/entity/PostLikeId.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Data
 @NoArgsConstructor
@@ -12,4 +13,17 @@ import java.io.Serializable;
 public class PostLikeId implements Serializable {
     private Long userId;
     private Long postId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PostLikeId that = (PostLikeId) o;
+        return Objects.equals(userId, that.userId) && Objects.equals(postId, that.postId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, postId);
+    }
 }
